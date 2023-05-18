185f26f509a89cdbf9146ef7cda7c825915637d4
OlegBrozhyna <olegbrogina@gmail.com>
2023-05-18 10:55:33 +0300

    update

--- src/main/java/com/example/myshop/controller/AdminController.java
+++ src/main/java/com/example/myshop/controller/AdminController.java
@@ -1,17 +1,25 @@
 package com.example.myshop.controller;
 
+import com.example.myshop.dto.ProductDTO;
 import com.example.myshop.model.Category;
 import com.example.myshop.service.CategoryService;
+import com.example.myshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
+    @Autowired
     private final CategoryService categoryService;
+    @Autowired
+    ProductService productService;
 
     @Autowired
     public AdminController(CategoryService categoryService) {
@@ -39,5 +47,36 @@ public class AdminController {
     public String postCatAdd(@ModelAttribute("category") Category category) {
         categoryService.addCategory(category);
         return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/admin/categories/delete/{id}")
+    public String deleteCat(@PathVariable long id) {
+        categoryService.removeCategoryById(id);
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/admin/categories/update/{id}")
+    public String updateCat(@PathVariable long id, Model model) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category.isPresent()) {
+            model.addAttribute("category", category.get());
+            return "categoriesAdd";
+        } else
+            return "404";
+    }
+
+    //Product Section
+
+    @GetMapping("/admin/products")
+    public String products(Model model) {
+        model.addAttribute("products", productService.getAllProduct());
+        return "products";
+    }
+
+    @GetMapping("/admin/products/add")
+    public String productAddGet(Model model) {
+        model.addAttribute("productDTO", new ProductDTO());
+        model.addAttribute("categories",categoryService.getAllCategory());
+        return "productsAdd";
     }
 }
--- src/main/java/com/example/myshop/model/Category.java
+++ src/main/java/com/example/myshop/model/Category.java
@@ -3,12 +3,13 @@ package com.example.myshop.model;
 import lombok.Data;
 
 import javax.persistence.*;
+
 @Entity
 @Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
- @Column(name = "category_id")
+    @Column(name = "category_id")
     private Long id;
     private String name;
 
