d6154ce0e9b7ab3b0e0bf45add7b68b843514a96
OlegBrozhyna <olegbrogina@gmail.com>
2023-06-03 13:45:51 +0300

    update

---
+++ src/main/java/com/example/myshop/model/OrderDetails.java
@@ -0,0 +1,24 @@
+package com.example.myshop.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "checkout")
+public class OrderDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String FirstName;
+    private String LastName;
+    private String Address;
+    private int ApartmentNumber;
+    private int Postcode;
+    private String City;
+    private int PhoneNumber;
+    private String Email;
+    private String Information;
+}
